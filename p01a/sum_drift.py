81876da229c1bd2b490c4b996bd5ebbf840184df
Jim Vanderveen <jim.vanderveen@ucop.edu>
2018-12-03 20:37:33 -0800

    Attempting Day 1 Part 2 - fail
    None of my resulting frequencies were repeated.
    Perhaps I should be keepting track of the change in frequency,
    rather than the resulting freq?

--- p01a/sum_drift.py
+++ p01a/sum_drift.py
@@ -4,7 +4,25 @@ infile = 'drift.txt'
 
 drift_h = open(infile, 'r')
 drift_list = drift_h.readlines()
+
 total_drift = 0
+
+# Now I need a dict to store the resulting freq, total_drift.
+# Report the first time a resulting freq is re-used.
+first_repeated_freq = None
+freq = dict()
+freq[total_drift] = 1    # This freq has appeared once.
+
 for d in drift_list:
     total_drift += int(d)
+    if total_drift in freq:
+        # This frequency has appeared before, increment counter.
+        freq[total_drift] += 1
+        if first_repeated_freq is None:
+            # First repeated frequency, report it!
+            print('First freq to repeat:', first_repeated_freq)
+    else:
+        # New freq, note its appearance.
+        ### print('    new freq:', total_drift)
+        freq[total_drift] = 1
 print('Total drift:', total_drift)
