3135aa7123db06efac4268b65ba4e03129a0fb67
Jim Vanderveen <jim.vanderveen@ucop.edu>
2018-12-04 06:26:13 -0800

    Day 1 Part 2 solved
    Treat the frequency drift as a circular list.

--- p01a/sum_drift.py
+++ p01a/sum_drift.py
@@ -2,27 +2,23 @@
 
 infile = 'drift.txt'
 
-drift_h = open(infile, 'r')
-drift_list = drift_h.readlines()
+with open(infile, 'r') as f:
+    drift_list = [int(x.strip()) for x in f.readlines()]
+resulting_freq = sum(drift_list)
+print('Resulting frequency:', resulting_freq)
 
-total_drift = 0
-
-# Now I need a dict to store the resulting freq, total_drift.
+# Now I need a dict to store the resulting freq, curr_freq.
 # Report the first time a resulting freq is re-used.
+curr_freq = 0
+freqs = {}
+freqs[curr_freq] = True
+d_index = 0    # Index of the circular drift list.
 first_repeated_freq = None
-freq = dict()
-freq[total_drift] = 1    # This freq has appeared once.
-
-for d in drift_list:
-    total_drift += int(d)
-    if total_drift in freq:
-        # This frequency has appeared before, increment counter.
-        freq[total_drift] += 1
-        if first_repeated_freq is None:
-            # First repeated frequency, report it!
-            print('First freq to repeat:', first_repeated_freq)
+while first_repeated_freq is None:
+    curr_freq += drift_list[d_index]
+    if curr_freq not in freqs:
+        freqs[curr_freq] = True
+        d_index = (d_index + 1) % len(drift_list)
     else:
-        # New freq, note its appearance.
-        ### print('    new freq:', total_drift)
-        freq[total_drift] = 1
-print('Total drift:', total_drift)
+        first_repeated_freq = curr_freq    # loop exit condition
+print('First freq to repeat:', first_repeated_freq)
